edf1dca12630e325cf471414ae7ced14849dcd6e
lk00607 <161328147+lk00607@users.noreply.github.com>
2024-02-26 15:56:31 +0000

    Update models_mae.py

--- models_mae.py
+++ models_mae.py
@@ -23,8 +23,8 @@ class MaskedAutoencoderViT(nn.Module):
     """ Masked Autoencoder with VisionTransformer backbone
     """
     def __init__(self, img_size=224, patch_size=16, in_chans=3,
-                 embed_dim=1024, depth=24, num_heads=16,
-                 decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+                 embed_dim=124, depth=24, num_heads=16,
+                 decoder_embed_dim=128, decoder_depth=8, decoder_num_heads=16,
                  mlp_ratio=8., norm_layer=nn.LayerNorm, norm_pix_loss=False):
         super().__init__()
 
